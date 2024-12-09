9e421810b45b1921be42f2603ed5bf0d4541d746
Vitor Emanuel Odorico Neto Costa <VCosta@solucaonetwork.com>
2024-12-09 17:02:37 -0300

    startProject

---
+++ src/main/java/io/github/cursodsouza/arquiteturasSpring/ArquiteturasSpringApplication.java
@@ -0,0 +1,13 @@
+package io.github.cursodsouza.arquiteturasSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/cursodsouza/arquiteturasSpring/ArquiteturasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursodsouza.arquiteturasSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
